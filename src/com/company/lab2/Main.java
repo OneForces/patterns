54a025d1076959f0da175afd9ad2515ab9dcbf8b
OneForces <teller2003@mail.ru>
2021-02-23 15:56:08 +0300

    add 2lab

---
+++ src/com/company/lab2/Main.java
@@ -0,0 +1,31 @@
+package com.company.lab2;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Human> humans = StreamAPIUtil.getHuman();
+        //Тестирование сортировки
+        System.out.println("Тестирование сортировки");
+        List<Human> sortedHumanAsc = StreamAPIUtil.sortHumanByAge(humans);
+        List<Human> sortedHumanDesc = StreamAPIUtil.sortHumanByAgeReversed(humans);
+        StreamAPIUtil.printHuman(sortedHumanAsc);
+        StreamAPIUtil.printHuman(sortedHumanDesc);
+        System .out.println("фильтрация по возрасту меньше, чем 20,");
+        // фильтрация по возрасту меньше, чем 20,
+        List<Human> AllHumanLessThen20 = StreamAPIUtil.AllHumanLessThen(humans,20);
+        StreamAPIUtil.printHuman(AllHumanLessThen20);
+
+        // фильтрация по имени «содержит ‘е’»,
+        System .out.println("фильтрация по имени «содержит ‘е’»");
+        List<Human> NameWithE = AllHumanWithSomeLetter(humans);
+        StreamAPIUtil.printHuman(NameWithE);
+        //конкатенация первых букв имен.
+        System.out.println(StreamAPIUtil.firstLettersOfTheName(humans));
+    }
+    public static List<Human> AllHumanWithSomeLetter(List<Human> humans){
+        return humans.stream().filter(i -> i.getFirstName().contains("e")).collect(Collectors.toList());
+    }
+}
---
+++ src/com/company/lab2/StreamAPIUtil.java
@@ -0,0 +1,58 @@
+package com.company.lab2;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+public class StreamAPIUtil {
+
+    public static List<Human> getHuman(){
+        List<Human> result = new ArrayList<>();
+        result.add(new Human(50, "Ivan", "Ivanov", LocalDate.of(1971,2,8), 85));
+        result.add(new Human(60, "Fedor", "Alexandrov",LocalDate.of(1961,3,7), 95));
+        result.add(new Human(34, "Alexander ", "Sergeev", LocalDate.of(1987,9,24), 77));
+        result.add(new Human(71, "Petr", "Fedorov", LocalDate.of(1950,4,16), 71));
+        result.add(new Human(19, "Dima", "Fedorov", LocalDate.of(2002,11,11), 67));
+        result.add(new Human(12, "Nikita", "Petrov", LocalDate.of(2009,9,24), 51));
+
+        return result;
+    }
+    public static List<String> getNameOfHuman(){
+        List<String> result = new ArrayList<>();
+        result.add("Ivan");
+        result.add("Fedor");
+        result.add("Alexander");
+        return result;
+    }
+
+    public static List<Human> sortHumanByAge(List<Human> h) {//По Возростанию
+        return h.stream()
+                .sorted(Comparator.comparing(Human::getAge))
+                .collect(Collectors.toList());
+    }
+
+    public static List<Human> sortHumanByAgeReversed(List<Human> h) {//По убыванию
+        return h.stream()
+                .sorted(Comparator.comparing(Human::getAge).reversed())
+                .collect(Collectors.toList());
+    }
+
+    public static List<Human> AllHumanLessThen(List<Human> humans, int age) {// фильтрация по возрасту меньше
+        return humans.stream().filter(i -> i.getAge() > age).collect(Collectors.toList());
+    }
+    public static String firstLettersOfTheName(List<Human> h){
+        AtomicReference<String> abgdeyozh = new AtomicReference<>("");
+        h.stream().forEach((x) -> {
+                    abgdeyozh.updateAndGet(v -> v + x.getFirstName().charAt(0));
+                }
+        );
+        return abgdeyozh.get();
+    }
+
+    // Вывод в консоль
+    public static void printHuman(List<Human> humans) {
+        humans.forEach(System.out::println);
+    }
+}
